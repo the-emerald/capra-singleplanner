bf824132dcf6723681a218a73211058ec1c5d4d2
Anson <git@anson-cheung.com>
2020-03-06 02:57:42 +0000

    Add basic working code

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 set -ex
+export RUST_BACKTRACE=1
 cargo run < src/sample.json
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ use serde_json::Value;
 use capra::common::dive_segment::{DiveSegment, SegmentType};
 use capra::common::gas::Gas;
 use serde::{Deserialize, Serialize};
+use capra::planner::plan_dive;
+use capra::zhl16::ZHL16;
+use capra::zhl16::util::{ZHL16B_N2_A, ZHL16B_N2_B, ZHL16B_N2_HALFLIFE, ZHL16B_HE_A, ZHL16B_HE_HALFLIFE, ZHL16B_HE_B};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct JSONDecoGas {
     o2: f64,
-    he: f64
+    he: f64,
+    modepth: Option<usize>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -16,7 +20,7 @@ struct JSONDiveSegment {
     depth: usize,
     time: usize,
     o2: f64,
-    he: f64
+    he: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -40,11 +44,11 @@ fn main() {
     }
 
     let mut bottom_segments: Vec<(DiveSegment, Gas)> = Vec::new();
-    let mut deco_mixes: Vec<Gas> = Vec::new();
+    let mut deco_mixes: Vec<(Gas, Option<usize>)> = Vec::new();
 
     let js: JSONDive = serde_json::from_str(&line).unwrap();
 
-    println!("{:?}", js);
+//    println!("{:?}", js);
 
     let ascent_rate = match js.asc {
         Some(t) => t,
@@ -67,7 +71,7 @@ fn main() {
     };
 
     for gas in js.deco_gases {
-        deco_mixes.push(Gas::new(1.0 - gas.he - gas.o2, gas.o2, gas.he).unwrap());
+        deco_mixes.push((Gas::new(1.0 - gas.he - gas.o2, gas.o2, gas.he).unwrap(), gas.modepth));
     }
 
     for seg in js.segments {
@@ -76,16 +80,16 @@ fn main() {
                                             descent_rate).unwrap(),
                            Gas::new(1.0 - seg.he - seg.o2, seg.o2, seg.he).unwrap()));
     }
-//
-//    println!("{:?}", bottom_segments);
-//    println!("{:?}", deco_mixes)
 
-//    println!("{:?}", v["gases"]);
-//    for x in v["gases"].as_array().unwrap() {
-//        let he = x["he"].as_f64().unwrap();
-//        let o2 = x["o2"].as_f64().unwrap();
-//        gases.push(Gas::new(1.0 - he - o2, o2, he).unwrap())
-//    }
+    let zhl16 = ZHL16::new(
+        &Gas::new(0.79, 0.21, 0.0).unwrap(),
+        ZHL16B_N2_A, ZHL16B_N2_B, ZHL16B_N2_HALFLIFE, ZHL16B_HE_A, ZHL16B_HE_B, ZHL16B_HE_HALFLIFE, gfl, gfh);
 
-
+    let plan = plan_dive(zhl16, &bottom_segments, &deco_mixes);
+    for x in plan {
+//        if x.0.get_segment_type() == SegmentType::AscDesc {
+//            continue;
+//        }
+        println!("{:?}: {}m for {}min - {}/{}", x.0.get_segment_type(), x.0.get_end_depth(), x.0.get_time(), x.1.fr_o2()*100.0, x.1.fr_he()*100.0);
+    }
 }
