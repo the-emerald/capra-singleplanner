e3eae8f01a9cb9f270129c6c9693ba6acc9e2e44
Anson <git@anson-cheung.com>
2020-04-05 22:35:12 +0800

    Update to use refactored planner

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use capra::planner::plan_dive;
 use capra::zhl16::ZHL16;
 use capra::zhl16::util::{ZHL16B_N2_A, ZHL16B_N2_B, ZHL16B_N2_HALFLIFE, ZHL16B_HE_A, ZHL16B_HE_HALFLIFE, ZHL16B_HE_B};
 
+const DEFAULT_GFL: usize = 100;
+const DEFAULT_GFH: usize = 100;
+
 #[derive(Serialize, Deserialize, Debug)]
 struct JSONDecoGas {
     o2: f64,
@@ -32,20 +35,17 @@ struct JSONDive {
     deco_gases: Vec<JSONDecoGas>
 }
 
-
-const DEFAULT_GFL: usize = 100;
-const DEFAULT_GFH: usize = 100;
 fn main() {
     let mut line: String = "".parse().unwrap();
     let stdin = io::stdin();
     for x in BufReader::new(stdin).lines() {
-        line = line.to_owned() + &x.unwrap() + "\n"
+        line = line.to_owned() + &x.expect("unable to read input") + "\n"
     }
 
     let mut bottom_segments: Vec<(DiveSegment, Gas)> = Vec::new();
     let mut deco_mixes: Vec<(Gas, Option<usize>)> = Vec::new();
 
-    let js: JSONDive = serde_json::from_str(&line).expect("Unable to decode user input");
+    let js: JSONDive = serde_json::from_str(&line).expect("unable to decode user input");
 
     let ascent_rate = match js.asc {
         Some(t) => t,
@@ -69,22 +69,22 @@ fn main() {
 
     for gas in js.deco_gases {
         deco_mixes.push((Gas::new(1.0 - gas.he - gas.o2, gas.o2, gas.he)
-                             .expect("Unable to decode decompression gas"), gas.modepth));
+                             .expect("unable to decode decompression gas"), gas.modepth));
     }
 
     for seg in js.segments {
         bottom_segments.push((DiveSegment::new(SegmentType::DiveSegment,
                                             seg.depth, seg.depth, seg.time, ascent_rate,
-                                            descent_rate).expect("Unable to decode segment"),
+                                            descent_rate).expect("unable to decode segment"),
                            Gas::new(1.0 - seg.he - seg.o2, seg.o2, seg.he)
-                               .expect("Unable to decode bottom gas")));
+                               .expect("unable to decode bottom gas")));
     }
 
-    let zhl16 = ZHL16::new(
+    let mut zhl16 = ZHL16::new(
         &Gas::new(0.79, 0.21, 0.0).unwrap(), // This shouldn't error
         ZHL16B_N2_A, ZHL16B_N2_B, ZHL16B_N2_HALFLIFE, ZHL16B_HE_A, ZHL16B_HE_B, ZHL16B_HE_HALFLIFE, gfl, gfh);
 
-    let plan = plan_dive(zhl16, &bottom_segments, &deco_mixes, ascent_rate, descent_rate);
+    let plan = plan_dive(&mut zhl16, &bottom_segments, &deco_mixes, ascent_rate, descent_rate);
     println!("Ascent rate: {}m/min", ascent_rate);
     println!("Descent rate: {}m/min", descent_rate);
     println!("GFL/GFH: {}/{}", gfl, gfh);
